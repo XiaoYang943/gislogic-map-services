9f2bf8156b515b6de04db81568203538f653d509
heyiyang <1352253543@qq.com>
2024-06-27 16:39:57 +0800

    feat: add flatgeobuf module

---
+++ flatgeobuf/src/main/java/org/gislogic/flatgeobuf/GeometryOffsets.java
@@ -0,0 +1,13 @@
+package org.gislogic.flatgeobuf;
+
+public class GeometryOffsets {
+    public int coordsOffset;
+    public int zOffset;
+    public int mOffset;
+    public long[] ends = null;
+    public int[] lengths = null;
+    public int endsOffset = 0;
+    public int lengthsOffset = 0;
+    public int type = 0;
+    public GeometryOffsets[] gos = null;
+}
